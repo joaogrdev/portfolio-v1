0fa784bc1b5e1afde7b3b7470afe569a1de35936
João Gabriel Ribeiro <95253789+joaogabrielr@users.noreply.github.com>
2022-08-28 17:07:45 -0300

    Mudei navbar

--- js/landpage.js
+++ js/landpage.js
@@ -1,7 +1,13 @@
 const menus = document.querySelectorAll(".container__hamburguer");
 let hamburguer = document.querySelector(".hamburguer")
 
+//fecha o menu, e destrava posição do botão
+function closeMenu() {
+    document.getElementById("myNav").style.height = "0%";
+    hamburguer.style.position = "relative";
+}
 
+//ao clicar em MENU liga e desliga classe OPEN, abrindo e fechando menu, e mudando design de hamburguer para X
 menus.forEach(function (menu) {
     menu.addEventListener('click', function() {
         hamburguer.classList.toggle("open");
@@ -10,9 +16,16 @@ menus.forEach(function (menu) {
             hamburguer.style.position = "fixed";
             hamburguer.style.maxWidth = "45px";
         } else {
-            document.getElementById("myNav").style.height = "0%";
-            hamburguer.style.position = "relative";
-        }
+            closeMenu();
+        } 
     })
 })
 
+const links = document.querySelectorAll(".overlay-content");
+//ao clicar em um link, fecha o menu, destrava botao, e volta para hamburguer
+links.forEach(function(link){
+    link.addEventListener('click', () => {
+        closeMenu();
+        hamburguer.classList.toggle("open");
+    })
+});
