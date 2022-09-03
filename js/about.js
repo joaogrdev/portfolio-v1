93f3e0d92eece6365c5843b984e6b568c1f589de
João Gabriel Ribeiro <95253789+joaogabrielr@users.noreply.github.com>
2022-09-02 23:49:36 -0300

    portfolio e pollos fritos

--- js/about.js
+++ js/about.js
@@ -1,28 +1,29 @@
-let bioParag = document.getElementsByClassName("bio__parag")[0];
-const shortAbout = document.getElementById("short-about");
-const mediumShortAbout = document.getElementById("mediumshort-about");
-const mediumLargeAbout = document.getElementById("mediumlarge-about");
-const largeAbout = document.getElementById("large-about");
+let bioParag = document.querySelector(".bio__parag");
+
+const bioOptions = ["<strong>João Gabriel</strong> faz o que gosta, e gosta do que faz.", 
+                    " Ele é formado em <span class='bio__even'>Biologia</span>, mas decidiu mudar de rota e se apaixonou pela programação.", 
+                    " João adora <span class='bio__odd'>resolver</span> problemas, é <span class='bio__even'>criativo</span> e busca <span class='bio__odd'>aprender</span> coisas novas ativa e constantemente. Preza pelo código claro e reutilizável, sem deixar a inovação de lado.", 
+                    " Seu objetivo é conquistar um espaço no mercado como <span class='bio__even'>Desenvolvedor Web</span> e participar de projetos que engrandeçam profissional e pessoalmente."];
 
 
 
-const bioOptions = ["João Gabriel faz o que gosta, e gosta do que faz.", 
-                    "João Gabriel faz o que gosta, e gosta do que faz. Ele é formado em Biologia, mas decidiu seguir o chamado da programação.", 
-                    "João Gabriel faz o que gosta, e gosta do que faz. Ele é formado em Biologia, mas decidiu seguir o chamado da programação. João é autodidata, e se considera uma pessoa criativa e que adora resolver problemas.", 
-                    "João Gabriel faz o que gosta, e gosta do que faz. Ele é formado em Biologia, mas decidiu seguir o chamado da programação. João é autodidata, e se considera uma pessoa criativa e que adora resolver problemas. Prezando pelo código claro e reutilizável, ele trilha o caminho DEV em busca da próxima oportunidade."];
+const boxes = document.querySelectorAll(".checkbox__input");
 
-shortAbout.onclick = () => {
-    bioParag.textContent = bioOptions[0];
-}
-
-mediumShortAbout.onclick = () => {
-    bioParag.textContent = bioOptions[1];
-}
-
-mediumLargeAbout.onclick = () => {
-    bioParag.textContent = bioOptions[2];
-}
-
-largeAbout.onclick = () => {
-    bioParag.textContent = bioOptions[3];
-}
+boxes.forEach(function(box) {
+    box.addEventListener('click', function(e) {
+        let boxChecked = e.currentTarget.classList;
+        if (boxChecked.contains("mediumshort")) {
+            bioParag.innerHTML = bioOptions[0] + bioOptions[1];
+            bioParag.style.height = "60px";
+        } else if(boxChecked.contains("mediumlarge")) {
+            bioParag.innerHTML = bioOptions[0] + bioOptions[1] + bioOptions[2];
+            bioParag.style.height = "120px";
+        } else if (boxChecked.contains("large")) {
+            bioParag.innerHTML = bioOptions[0] + bioOptions[1] + bioOptions[2] + bioOptions[3];
+            bioParag.style.height = "190px";
+        } else {
+            bioParag.innerHTML = bioOptions[0];
+            bioParag.style.height = "20px";
+        }
+    })
+})
---
+++ projetos/los-pollos-fritos/js/landing-page.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector(".header__minmax");
+
+let minmax = "max";
+btn.addEventListener('click', function() {
+    if (minmax === "max") {
+        document.querySelector(".header").style.width = "50px";
+        document.querySelector(".header").style.overflow = "hidden";
+        document.querySelector(".header").style.transitionDuration = ".7s";
+
+        document.querySelector(".content").style.width = "calc(100vw - 50px)"
+        minmax = "min";
+    } else {
+        document.querySelector(".header").style.width = "25%";
+        document.querySelector(".header").style.opacity = "1";
+        minmax = "max";
+    }
+})
