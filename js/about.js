0bf3f775ed0139c21ffd90e832481c2cc0894649
joao.sousa <joao.sousa@thinklife.com.br>
2025-06-10 19:46:47 -0300

    fix: ajustando novidades no portfolio

--- js/about.js
+++ js/about.js
@@ -1,9 +1,9 @@
 let bioParag = document.querySelector(".bio__parag");
 
 const bioOptions = ["<strong>João Gabriel</strong> faz o que gosta, e gosta do que faz.", 
-                    " Ele é formado em <span class='bio__even'>Biologia</span>, mas decidiu mudar de rota e se apaixonou pela programação.", 
+                    " Ele é formado em ADS e trabalha como desenvolvedor <span class='bio__even'>front-end</span> há 3 anos.", 
                     " João adora <span class='bio__odd'>resolver</span> problemas, é <span class='bio__even'>criativo</span> e busca <span class='bio__odd'>aprender</span> coisas novas ativa e constantemente. Preza pelo código claro e reutilizável, sem deixar a inovação de lado.", 
-                    " Seu objetivo é conquistar um espaço no mercado como <span class='bio__even'>Desenvolvedor Web</span> e participar de projetos que engrandeçam profissional e pessoalmente."];
+                    " Seu objetivo é conquistar espaço, se tornando <span class='bio__even'>referência no desenvolvimento</span> e contribuindo em projetos que possam melhorar a vida das pessoas."];
 
 
 
