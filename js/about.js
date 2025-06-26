a399c5eeb31f06b1d77e27a3dfe6631addedc711
joao.sousa <joao.sousa@thinklife.com.br>
2025-06-26 11:02:41 -0300

    feat: atualizando o nome para fullstack

--- js/about.js
+++ js/about.js
@@ -1,7 +1,7 @@
 let bioParag = document.querySelector(".bio__parag");
 
 const bioOptions = ["<strong>João Gabriel</strong> faz o que gosta, e gosta do que faz.", 
-                    " Ele é formado em ADS e trabalha como desenvolvedor <span class='bio__even'>front-end</span> há 3 anos.", 
+                    " Ele é formado em ADS e trabalha como desenvolvedor <span class='bio__even'>fullstack</span> há 3 anos.", 
                     " João adora <span class='bio__odd'>resolver</span> problemas, é <span class='bio__even'>criativo</span> e busca <span class='bio__odd'>aprender</span> coisas novas ativa e constantemente. Preza pelo código claro e reutilizável, sem deixar a inovação de lado.", 
                     " Seu objetivo é conquistar espaço, se tornando <span class='bio__even'>referência no desenvolvimento</span> e contribuindo em projetos que possam melhorar a vida das pessoas."];
 
